72002581d5f1a59e0df3ea0e1c3a867fe5eb1ba5
isida2 <c231006@ivy.ac.jp>
2025-09-30 14:53:00 +0900

    unity追加

---
+++ My project (3)/Assets/Script/NewMonoBehaviourScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W key is being held down.");
+        }
+    }
+}
